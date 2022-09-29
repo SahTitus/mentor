dfbe3f03f4d41b6e1ef7dd71f7c3f03d026a762c
SahTitus <sahtitus58@gmail.com>
2022-09-29 17:39:42 +0000

    updates

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000"});
+const API = axios.create({ baseURL: "https://mentor-engine.vercel.app"});
 
 // const url = "http://localhost:5000"
 // "https://mentor-engine.vercel.app"
