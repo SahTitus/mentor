6f2869847ddc9e3e150532a1ba2d8af55ad52f36
SahTitus <sahtitus58@gmail.com>
2022-10-01 10:34:57 +0000

    updates

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000"});
-
-// const url = "http://localhost:5000"
-// "https://mentor-engine.vercel.app"
-
+const API = axios.create({ baseURL: "https://mentor-engine.vercel.app"});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
